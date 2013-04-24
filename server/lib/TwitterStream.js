e519b82873d95b6ea446d1089603f339dd44ffa2
Florian Demmer <demmer.florian@googlemail.com>
2013-04-24 14:30:01 +0200

    added modules for accessing the Twitter Streaming API. This included a helper to read from an incoming stream and the "TwitterStream", which connects to Twitter using OAuth and listens for incoming chunks, which are then emitted as a "tweet" event

---
+++ server/lib/TwitterStream.js
@@ -0,0 +1,76 @@
+var util = require('util')
+	, events = require('events')
+	, OAuth = require('oauth').OAuth
+	, StreamReader = require('./streamReader')
+
+module.exports = TwitterStream;
+
+function TwitterStream(options){
+
+	if(!options.consumerKey || !options.consumerSecret || !options.accessToken || !options.accessSecret){
+		// throw error
+		console.log("incomplete!");
+	}
+
+    events.EventEmitter.call(this);
+
+    this.url = "https://stream.twitter.com/1.1/statuses/filter.json?delimited=length&track=Berlin";
+    this.accessToken = options.accessToken;
+    this.accessSecret = options.accessSecret;
+    
+    this.streamReader = new StreamReader({
+		'sizeLength' : 6
+    	, 'sizeReader' : function(buffer){
+    		return parseInt(buffer.toString('utf8',0,4));
+    	}
+	});
+
+    this.registerDataHandler();
+
+	this.twitterLogin = new OAuth(
+		"https://twitter.com/oauth/request_token"
+        , "https://twitter.com/oauth/access_token"
+        , options.consumerKey
+        , options.consumerSecret
+        , "1.0A"
+        , null
+        , "HMAC-SHA1"
+    );
+}
+
+util.inherits(TwitterStream, events.EventEmitter);
+
+TwitterStream.prototype.connect = function(query){
+	if(this.request != undefined){
+		this.abort();
+	}
+
+	this.request = this.twitterLogin.get(this.url, this.accessToken, this.accessSecret);
+
+	var self = this;
+	self.request.on('response',function(response){
+		response.on('data',function(chunk){
+			self.streamReader.read(chunk);
+		});
+	});
+	self.request.end();
+};
+
+TwitterStream.prototype.registerDataHandler = function(){
+	var self = this;
+	this.streamReader.on('data',function(data){
+		data = data.toString('utf8',6);
+		try{
+			var json = JSON.parse(data);
+			self.emit('tweet',json);
+		} catch(err){
+			console.log(err);
+		}
+	});
+}
+
+TwitterStream.prototype.abort = function(){
+	if(this.request) this.request.abort();
+	return;
+}
+
