8b1bdc301ee00e2a365647ecca7665d3c587ab21
Florian Demmer <demmer.florian@googlemail.com>
2013-05-06 10:26:21 +0200

    fixed buggy page alerts, which so far only occured once or were shown underneath each other. On upload the button is also switched to 'loading' state and reset afterwards.

--- server/public/js/admin/app.js
+++ server/public/js/admin/app.js
@@ -2,19 +2,32 @@ $(function(){
 
 	var $progressBar = $('#progress_bar');
 	var $progressBarContainer = $('#progress_bar_container');
-	var $uploadInfo = $('#upload_info');
-	var $pageAlert = $('#page_alert').alert();
+	
+	var $pageAlert = $('<div>').attr('class','page-alert').alert();
+	var $pageAlertCloser = $('<a>')
+		.attr({
+			'class' : 'close'
+			, 'data-dismiss' : 'alert'
+			, 'href' : '#'
+		})
+		.text('x');
 
 	function showUploadInfo(success,message){
-		var cssClass = success ? 'alert alert-success' : 'alert alert-error';
-		$uploadInfo.attr('class',cssClass);
-		$uploadInfo.children('p').text(message);
-		$pageAlert.show();
+
+		$pageAlert.empty();
+		var $pageAlertText = $('<div>').appendTo($pageAlert);
+		$pageAlertText.attr('class',success ? 'alert alert-success' : 'alert alert-error');
+		$pageAlertText.text(message)
+		$pageAlertCloser.appendTo($pageAlertText);
+		
+		$pageAlert.prependTo('body');
 		$progressBarContainer.hide();
 	}
 
 	$('#submit_file').on('click',function(event){
 		event.preventDefault();
+		
+		var self = $(this).button('loading');
 
 		$progressBar.css('width','0%');
 		$progressBarContainer.show();
@@ -32,11 +45,13 @@ $(function(){
 
 		xhr.onerror = function(e){
 			showUploadInfo(false,'An error occured while submitting the file!');
+			self.button('reset');
 		};
 
 		xhr.onload = function(){
 			var success = this.status === 200;
 			showUploadInfo(success,this.response);
+			self.button('reset');
 		};
 
 		xhr.send(formData);
