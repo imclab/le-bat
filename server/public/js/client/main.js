86abc9cc82f35f69bbd24e3c06199440e95b3567
Florian Demmer <demmer.florian@googlemail.com>
2013-05-07 17:52:18 +0200

    redesigned client to now use require.js based modules and changed the client view according to that. The Locator is now also able to calculate the distance and the bearing to given lat and lon coordinates

---
+++ server/public/js/client/main.js
@@ -0,0 +1,11 @@
+require.config({
+	//baseUrl : '',
+	paths : {
+		'domReady' : '/js/libs/require/plugins/domReady'
+	},
+
+});
+
+require(['domReady','app'],function(domReady,app){
+	domReady(app.init);
+});
---
+++ server/public/js/client/websocketConnection.js
@@ -0,0 +1,47 @@
+define([
+	'eventEmitter'
+],function(EventEmitter){
+
+	//Wrapper for websocket in order encapsulate parsing, reconnections etc.
+	function WebsocketConnection(url){
+
+		EventEmitter.call(this);
+		this.url = url;
+
+		this.connect = function(){
+			this.socket = new WebSocket(this.url);
+
+			var self = this;
+			
+			this.socket.onopen = function(){
+				self.emit('connected');
+			};
+
+			this.socket.onerror = function(err){
+				console.log(err);
+				// reconnect
+				self.connect();
+			};
+
+			this.socket.onclose = function(){
+				self.emit('close');
+			};
+
+			this.socket.onmessage = function(msg){
+				try{
+					var json = JSON.parse(msg.data);
+					self.emit('data',json);
+				} catch(e){
+					console.error('error while parsing incoming message');
+				}
+			};
+		};
+	}
+
+	// inherit from EventEmitter
+	WebsocketConnection.prototype = Object.create(EventEmitter.prototype);
+
+	return WebsocketConnection;
+});
+
+
