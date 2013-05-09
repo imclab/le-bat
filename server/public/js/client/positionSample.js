8babb800d42cbf9b63bff6913313defb78131c74
Florian Demmer <demmer.florian@googlemail.com>
2013-05-09 12:11:39 +0200

    added the missing PositionSample module which holds a PannerNode for positioning the sound in space. On play it creates a new AudioBufferSourceNode, positions the sound and plays it. Although creating a new BufferSourceNode for each playback might sound inefficient it actually isn't. You can only play a BufferSourceNode once and it's furthermore heavily designed to be created often, fast and high performant.

---
+++ server/public/js/client/positionSample.js
@@ -0,0 +1,54 @@
+define([
+],function(){
+
+	function PositionSample(buffer,destination,context){
+
+		this.buffer = buffer;
+		this.context = context;
+
+		// create a PannerNode and connect it to the given
+		// destination
+		this.panner = context.createPanner();
+		this.panner.connect(destination);
+
+	}
+
+	PositionSample.prototype.play = function(x,y,z){
+
+		x = x * 100;
+		z = z * 100;
+
+		// create a AudioBufferSourceNode from the given buffer
+		// this might sound inefficient but it's actually the
+		// way to do it.
+		// 
+		// a more official statement can be found here:
+		// http://updates.html5rocks.com/2012/01/Web-Audio-FAQ
+
+		this.source = this.context.createBufferSource();
+		this.source.buffer = this.buffer;
+
+		// connect the buffer to the panner
+		this.source.connect(this.panner);
+		
+		// set the panner's position to the given coordinates
+		this.panner.setPosition(x,y,z);
+
+		// set the panner's position so it points to (0,0,0)
+		this.panner.setOrientation(-x,-y,-z);
+
+		this.source.start(0);
+	};
+
+	PositionSample.prototype.isPlaying = function(){
+
+		// check if we actually ever played a sound
+		if(!this.source) return false;
+		// check if our AudioBufferSource is done playing
+		if(this.source.playbackState == 3) return false;
+		// we are free
+		return true;
+	}
+
+	return PositionSample;
+});
