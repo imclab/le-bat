e5620be3d1dcb1d192d009160f6ea89dfaaf2074
Florian Demmer <demmer.florian@googlemail.com>
2013-05-13 18:46:53 +0200

    landing page: render the latest updated 10 sets in a table

--- frontend/routes/client/main.js
+++ frontend/routes/client/main.js
@@ -1,5 +1,6 @@
 var async = require('async')
-,	SequenceSoundSet = require('../../../model/SequenceSoundSet');
+,	SequenceSoundSet = require('../../../model/SequenceSoundSet')
+,	User = require('../../../model/user');
 
 module.exports.index = function(req,res,next){
 	if(!req.db || !req.db.ready)
@@ -10,9 +11,9 @@ module.exports.index = function(req,res,next){
 			done(null,req,res);
 		}
 		, getSets
+		, populateSetsWithNames
 	], function(err){
 		res.locals.pagetitle = 'Le social sound';
-		res.locals.sets = [];
 		res.render('client/landing');
 	});
 };
@@ -20,10 +21,28 @@ module.exports.index = function(req,res,next){
 function getSets(req,res,done){
 	req.db.getAll(SequenceSoundSet.ModelInfo,{ order : [{col : 'updated' , desc : true}] , limit : {count : 10} }, function(err, result) {
 		if(err) return res.send(500, err);
-		done(null, req, result);
+		res.locals.sets = result;
+		done(null, req, res);
 	});
 }
 
-function populateSetsWithNames(){
+function populateSetsWithNames(req,res,done){
+	var options = { where: [] };
+	res.locals.sets.forEach(function(set) {
+		options.where.push({
+			col: 'id',
+			val: set.user_id
+		});
+		options.where.push('or');
+	});
+	options.where.pop();
 
+	req.db.getAll(User.ModelInfo,options,function(err,result){
+		if(err) return res.send(500,err);
+		res.locals.users = {};
+		result.forEach(function(user){
+			res.locals.users[user.id] = User.strippedFromObject(user);
+		});
+		return done(null);
+	});
 }
--- model/User.js
+++ model/User.js
@@ -15,6 +15,10 @@ User.fromObject = function(obj) {
 	return new User(obj.id, obj.auth_type, obj.name, obj.pass, obj.salt, obj.created);
 }
 
+User.strippedFromObject = function(obj){
+	return new User(obj.id,null,obj.name,null,null,null);
+}
+
 User.ModelInfo = new Database.ModelInfo({
 	tableName: 'user',
 	primary: 'id',
